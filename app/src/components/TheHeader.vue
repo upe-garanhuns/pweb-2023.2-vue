de8ca78983d031bb024ddc0ca8dc895174f4336f
Mylena Mahatma <mylenamahatma@gmail.com>
2024-03-08 07:05:29 -0300

    Ciclo de vida

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -1,19 +1,36 @@
 <template>
     <header class="header">
-        The Header
+        Header
     </header>
 </template>
 
 <script>
-     export default {
+    export default {
+        mounted() {
+            window.addEventListener('resize', this.resize)
+        },
+        beforeUnmount() {
+            // Destruir as nossas libs
+            // Eventos
+            // Listeners
+            console.log('beforeUnmount');
+            window.removeEventListener('resize', this.resize)
+        },
+        unmounted() {
+            console.log('unmounted');
+        },
 
-
-     }
+        methods: {
+            resize($evt) {
+                console.log($evt);
+            }
+        }
+    }
 </script>
 
 <style>
-     .header {
+    .header {
         background: #000;
         color: #fff;
-     }
+    }
 </style>
