fb54965d6a7e752f30a3a8fb337f248a44ac3ab9
Mylena Mahatma <mylenamahatma@gmail.com>
2024-03-07 22:18:18 -0300

    feat: componentes de arquivo

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,20 @@
 <template>
+ <TheHeader>
+
+ </TheHeader>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
   }
 }
 </script>
---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header">
+        The Header
+    </header>
+</template>
+
+<script>
+     export default {
+
+
+     }
+</script>
+
+<style>
+     .header {
+        background: #000;
+        color: #fff;
+     }
+</style>
